0f67e6759c009e811f509dfbbc7f0efd601b4d7c
MTJB <markbrown04@googlemail.com>
2022-02-13 22:08:31 +0000

    Parse link to message, add reactions from config file

--- react.py
+++ react.py
@@ -1,33 +1,56 @@
 #!/usr/bin/python3
-from slack_sdk import WebClient
-import os
-import sys
 import argparse
-from dotenv import load_dotenv
+import json
+import os
+import re
+import sys
 
-# Enable debug logging
-import logging
-logging.basicConfig(level=logging.DEBUG)
+from dotenv import load_dotenv
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 
 def main():
-
     load_dotenv()
     args = parse_args()
-    print(getFromEnv('SLACK_API_TOKEN'))
-    print(args.group)
-    # client = WebClient()
-    # api_response = client.api_test()
+
+    with open('emoji_groups.json', 'r') as f:
+        groups = json.load(f)
+
+    channel, timestamp = parse_link(args.link)
+
+    client = WebClient(token=get_from_env('SLACK_API_TOKEN'))
+
+    if args.group in groups:
+        for emoji in groups[args.group]:
+            try:
+                client.reactions_add(
+                    channel=channel,
+                    name=emoji,
+                    timestamp=timestamp
+                )
+            except SlackApiError as e:
+                if e.response.data['error'] != 'already_reacted':
+                    raise
+    else:
+        print('Emoji Group not found.')
 
 
-def getFromEnv(key: str):
+def get_from_env(key: str):
     try:
         value = os.environ[key]
     except KeyError:
         print(f'No environment variable found for key: [{key}]')
         sys.exit(1)
-    
+
     return value
+
+
+def parse_link(link):
+    channel = re.findall('[^archives]*(/.*?/)', link)[1][1:-1]
+    timestamp = re.findall('([^/]+$)', link)[0][1:]
+    timestamp = timestamp[:10] + '.' + timestamp[10:]
+    return channel, timestamp
 
 
 def parse_args() -> argparse.Namespace:
@@ -37,6 +60,10 @@ def parse_args() -> argparse.Namespace:
 
     parser.add_argument('--group', type=str, dest='group',
                         required=True, help='Name of the emoji group to apply')
+
+    # https://twitter.com/SlackAPI/status/911258119099092992
+    parser.add_argument('--link', type=str, dest='link',
+                        required=True, help='The link to the message you wish to react to')
 
     return parser.parse_args()
 
